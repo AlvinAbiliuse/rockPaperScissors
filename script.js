a985fb44e92025ea4a2fd33229d5818d92466d58
AlvinAbiluse <alvinabiliuse@gmail.com>
2024-12-13 21:08:09 +0400

    completed ui for rock paper scissors game

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-
-function determineChoice(n) {
+function getComputerChoice() {
+	let n = Math.floor(Math.random() * 3);
 	if (n == 0) {
 		return "rock";
 	} else if (n == 1) {
@@ -9,62 +9,80 @@ function determineChoice(n) {
 	}
 }
 
-function getComputerChoice() {
-	let randomNum = Math.floor(Math.random() * 3);
-	return determineChoice(randomNum);
-}
-
-function getHumanChoice() {
-	/* let answer = prompt("1 for rock, 2 for paper and 3 for scissors");
-	return determineChoice(answer - 1); */
-}
-
 function playRound(humanChoice, computerChoice) {
-	let choiceWeight = {	
-		rock: { beat: "scissors", lose: "paper"},
-		paper: {beat: "rock", lose: "scissors"},
-		scissors: {beat: "paper", lose: "rock"}
-	}
 	if (humanChoice == computerChoice) {
-		console.log("It's a tie");
-		return;
+		return "It's a tie";
 	} else if (humanChoice == "rock") {
 		if (computerChoice == "paper") {
-			console.log("You lose, Paper beats rock");
-			computerScore++
+			computerScore++;
+			count++;
+			return "You lose, Paper beats rock";
 		} else if (computerChoice == "scissors") {
-			console.log("You win, Rock beats Scissors");
 			humanScore++;
+			count++;
+			return "You win, Rock beats Scissors";
 		}
 	} else if (humanChoice == "paper") {
 		if (computerChoice == "rock") {
-			console.log("You win, Paper beats Rock");
 			humanScore++;
+			count++;
+			return "You win, Paper beats Rock";
 		} else if (computerChoice == "scissors") {
-			console.log("You lose, Scissors beat paper");
 			computerScore++;
+			count++;
+			return "You lose, Scissors beats paper";
 		}
 	} else if (humanChoice == "scissors") {
 		if (computerChoice == "rock") {
 			computerScore++;
+			count++;
+			return "You Lose, Rock beats scissors";
 		} else if (computerChoice == "paper") {
-			console.log("You win, Scissors beat paper");
 			humanScore++;
+			count++;
+			return "You win, Scissors beats paper";
 		}
 	}
-	count++
 }	
+
+function playGame(human, comp) {
+	let message = playRound(human, comp); 
+	if (count == 5) {
+		message = "Game Over! Your Score: " + humanScore + 
+				"    Computer Score: " + computerScore;
+		count = 0;
+		humanScore = 0;
+		computerScore = 0;
+		return message;
+	} else {
+		return message;
+	}
+}
+
+const checkEvent = (e) => {
+	if (e.target.nodeName == "BUTTON") {
+		let comp = getComputerChoice();
+		let msg = playGame(
+			e.srcElement.textContent.toLowerCase(), comp);
+		computerScoreEl.textContent = computerScore;
+		humanScoreEl.textContent = humanScore;
+		msgEl.textContent = msg;
+	}
+}
+
 	
 let humanScore = 0;
 let computerScore = 0;
 let count = 0;
 
-function playGame(n) {
-	while (count < n) {
-		playRound(getHumanChoice(), getComputerChoice());
-		console.log("Your Score: " + humanScore);
-		console.log("Computer Score: " + computerScore);
-	}
-}
+const computerScoreEl = document.querySelector("#computerScore");
+const humanScoreEl = document.querySelector("#humanScore");
+const msgEl = document.querySelector("#message");
+const btn = document.querySelector("#choices");
 
-playGame(1) /* prompt("How many points do you want to play for (number)")); */
+btn.addEventListener("click", checkEvent);
+
+
+
+
+
